9618ef4afb25950e3216af4d513db06fe8cb8c1d
Pan He <cndy1860@gmail.com>
2019-07-30 03:23:04 +0800

    init

---
+++ Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace xxEncryptTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBoxLoadFile = new System.Windows.Forms.RichTextBox();
+            this.buttonLoadFile = new System.Windows.Forms.Button();
+            this.buttonEncrypt = new System.Windows.Forms.Button();
+            this.richTextBoxEncryptFile = new System.Windows.Forms.RichTextBox();
+            this.labelTips1 = new System.Windows.Forms.Label();
+            this.labelTips2 = new System.Windows.Forms.Label();
+            this.labelTips0 = new System.Windows.Forms.Label();
+            this.labelTips3 = new System.Windows.Forms.Label();
+            this.labelTips4 = new System.Windows.Forms.Label();
+            this.buttonRecover = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBoxLoadFile
+            // 
+            this.richTextBoxLoadFile.Location = new System.Drawing.Point(48, 56);
+            this.richTextBoxLoadFile.Name = "richTextBoxLoadFile";
+            this.richTextBoxLoadFile.Size = new System.Drawing.Size(252, 270);
+            this.richTextBoxLoadFile.TabIndex = 0;
+            this.richTextBoxLoadFile.Text = "";
+            // 
+            // buttonLoadFile
+            // 
+            this.buttonLoadFile.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.buttonLoadFile.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.buttonLoadFile.Location = new System.Drawing.Point(48, 12);
+            this.buttonLoadFile.Name = "buttonLoadFile";
+            this.buttonLoadFile.Size = new System.Drawing.Size(110, 38);
+            this.buttonLoadFile.TabIndex = 2;
+            this.buttonLoadFile.Text = "载入文件";
+            this.buttonLoadFile.UseVisualStyleBackColor = true;
+            this.buttonLoadFile.Click += new System.EventHandler(this.ButtonLoadFile_Click);
+            // 
+            // buttonEncrypt
+            // 
+            this.buttonEncrypt.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.buttonEncrypt.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.buttonEncrypt.Location = new System.Drawing.Point(372, 12);
+            this.buttonEncrypt.Name = "buttonEncrypt";
+            this.buttonEncrypt.Size = new System.Drawing.Size(133, 38);
+            this.buttonEncrypt.TabIndex = 4;
+            this.buttonEncrypt.Text = "开始加密";
+            this.buttonEncrypt.UseVisualStyleBackColor = true;
+            this.buttonEncrypt.Click += new System.EventHandler(this.ButtonEncrypt_Click);
+            // 
+            // richTextBoxEncryptFile
+            // 
+            this.richTextBoxEncryptFile.Location = new System.Drawing.Point(372, 56);
+            this.richTextBoxEncryptFile.Name = "richTextBoxEncryptFile";
+            this.richTextBoxEncryptFile.Size = new System.Drawing.Size(401, 270);
+            this.richTextBoxEncryptFile.TabIndex = 3;
+            this.richTextBoxEncryptFile.Text = "";
+            // 
+            // labelTips1
+            // 
+            this.labelTips1.AutoSize = true;
+            this.labelTips1.Font = new System.Drawing.Font("宋体", 9F);
+            this.labelTips1.Location = new System.Drawing.Point(46, 365);
+            this.labelTips1.Name = "labelTips1";
+            this.labelTips1.Size = new System.Drawing.Size(65, 12);
+            this.labelTips1.TabIndex = 5;
+            this.labelTips1.Text = "labelTips1";
+            // 
+            // labelTips2
+            // 
+            this.labelTips2.AutoSize = true;
+            this.labelTips2.Font = new System.Drawing.Font("宋体", 9F);
+            this.labelTips2.Location = new System.Drawing.Point(46, 386);
+            this.labelTips2.Name = "labelTips2";
+            this.labelTips2.Size = new System.Drawing.Size(65, 12);
+            this.labelTips2.TabIndex = 6;
+            this.labelTips2.Text = "labelTips2";
+            // 
+            // labelTips0
+            // 
+            this.labelTips0.AutoSize = true;
+            this.labelTips0.Font = new System.Drawing.Font("宋体", 9F);
+            this.labelTips0.Location = new System.Drawing.Point(46, 343);
+            this.labelTips0.Name = "labelTips0";
+            this.labelTips0.Size = new System.Drawing.Size(65, 12);
+            this.labelTips0.TabIndex = 7;
+            this.labelTips0.Text = "labelTips0";
+            // 
+            // labelTips3
+            // 
+            this.labelTips3.AutoSize = true;
+            this.labelTips3.Font = new System.Drawing.Font("宋体", 9F);
+            this.labelTips3.Location = new System.Drawing.Point(46, 408);
+            this.labelTips3.Name = "labelTips3";
+            this.labelTips3.Size = new System.Drawing.Size(65, 12);
+            this.labelTips3.TabIndex = 8;
+            this.labelTips3.Text = "labelTips3";
+            // 
+            // labelTips4
+            // 
+            this.labelTips4.AutoSize = true;
+            this.labelTips4.Font = new System.Drawing.Font("宋体", 9F);
+            this.labelTips4.Location = new System.Drawing.Point(46, 429);
+            this.labelTips4.Name = "labelTips4";
+            this.labelTips4.Size = new System.Drawing.Size(65, 12);
+            this.labelTips4.TabIndex = 9;
+            this.labelTips4.Text = "labelTips4";
+            // 
+            // buttonRecover
+            // 
+            this.buttonRecover.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.buttonRecover.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.buttonRecover.Location = new System.Drawing.Point(640, 12);
+            this.buttonRecover.Name = "buttonRecover";
+            this.buttonRecover.Size = new System.Drawing.Size(133, 38);
+            this.buttonRecover.TabIndex = 10;
+            this.buttonRecover.Text = "还原备份";
+            this.buttonRecover.UseVisualStyleBackColor = true;
+            this.buttonRecover.Click += new System.EventHandler(this.ButtonRecover_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonRecover);
+            this.Controls.Add(this.labelTips4);
+            this.Controls.Add(this.labelTips3);
+            this.Controls.Add(this.labelTips0);
+            this.Controls.Add(this.labelTips2);
+            this.Controls.Add(this.labelTips1);
+            this.Controls.Add(this.buttonEncrypt);
+            this.Controls.Add(this.richTextBoxEncryptFile);
+            this.Controls.Add(this.buttonLoadFile);
+            this.Controls.Add(this.richTextBoxLoadFile);
+            this.Name = "Form1";
+            this.Text = "XX ENCRYPT TOOL";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBoxLoadFile;
+        private System.Windows.Forms.Button buttonLoadFile;
+        private System.Windows.Forms.Button buttonEncrypt;
+        private System.Windows.Forms.RichTextBox richTextBoxEncryptFile;
+        private System.Windows.Forms.Label labelTips1;
+        private System.Windows.Forms.Label labelTips2;
+        private System.Windows.Forms.Label labelTips0;
+        private System.Windows.Forms.Label labelTips3;
+        private System.Windows.Forms.Label labelTips4;
+        private System.Windows.Forms.Button buttonRecover;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace xxEncryptTool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            labelTips0.Text = "1.确保电脑上已经安装好lua5.1(叉叉仅支持5.1)，加密前建议手动备份";
+            labelTips1.Text = "2.先选择需要加密的文件(*.lua)，点击加密即可生成加密文件(*_.lua)";
+            labelTips2.Text = "3.对于加密后的源文件*.lua，均移动到'我的文档/xspBackup'目录下作备份，原目录只保留加密后的*_.lua";
+            labelTips3.Text = @"4.在main.lua首行reqiure encrypt，加密过的文件，使用encrequire代替require即可";
+            labelTips4.Text = "5.还原备份功能会将'我的文档/xspBackup'中的文件移动至项目文件夹";
+        }
+
+        public string[] filePaths = new string[1024];
+
+        bool executeCmd(string cmd)
+        {
+            System.Diagnostics.Process p = new System.Diagnostics.Process();
+            p.StartInfo.FileName = "cmd.exe";
+            p.StartInfo.UseShellExecute = false;    //是否使用操作系统shell启动
+            p.StartInfo.RedirectStandardInput = true;//接受来自调用程序的输入信息
+            p.StartInfo.RedirectStandardOutput = true;//由调用程序获取输出信息
+            p.StartInfo.RedirectStandardError = true;//重定向标准错误输出
+            p.StartInfo.CreateNoWindow = true;//不显示程序窗口
+            p.Start();//启动程序
+
+            //向cmd窗口发送输入信息
+            p.StandardInput.WriteLine(cmd + " &exit");
+
+            p.StandardInput.AutoFlush = true;
+            //p.StandardInput.WriteLine("exit");
+            //向标准输入写入要执行的命令。这里使用&是批处理命令的符号，表示前面一个命令不管是否执行成功都执行后面(exit)命令，如果不执行exit命令，后面调用ReadToEnd()方法会假死
+            //同类的符号还有&&和||前者表示必须前一个命令执行成功才会执行后面的命令，后者表示必须前一个命令执行失败才会执行后面的命令
+
+
+
+            //获取cmd窗口的输出信息
+            string output = p.StandardOutput.ReadToEnd();
+
+            //StreamReader reader = p.StandardOutput;
+            //string line=reader.ReadLine();
+            //while (!reader.EndOfStream)
+            //{
+            //    str += line + "  ";
+            //    line = reader.ReadLine();
+            //}
+
+            p.WaitForExit();//等待程序执行完退出进程
+            p.Close();
+
+
+            Console.WriteLine(output);
+
+            return true;
+        }
+
+        private void ButtonLoadFile_Click(object sender, EventArgs e)
+        {
+            //定义一个文件打开控件
+            OpenFileDialog ofd = new OpenFileDialog();
+            //设置打开对话框的初始目录，默认目录为exe运行文件所在的路径
+            ofd.InitialDirectory = Application.StartupPath;
+            //设置打开对话框的标题
+            ofd.Title = "请选择需要加密的文件";
+            //设置打开对话框可以多选
+            ofd.Multiselect = true;
+            //设置对话框打开的文件类型
+            ofd.Filter = "lua文件|*.lua";
+            //设置文件对话框当前选定的筛选器的索引
+            ofd.FilterIndex = 2;
+
+            ofd.InitialDirectory = System.Environment.CurrentDirectory + "\\xspworkspace";
+            //设置对话框是否记忆之前打开的目录
+            ofd.RestoreDirectory = false;
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                Array.Clear(filePaths, 0, filePaths.Length);
+                Array.Copy(ofd.FileNames, filePaths, ofd.FileNames.Length);
+                richTextBoxLoadFile.Text = "";
+                richTextBoxEncryptFile.Text = "";
+                foreach (string s in ofd.SafeFileNames)
+                {
+                    richTextBoxLoadFile.AppendText(s + "\r\n");
+                }
+
+                foreach (string s in ofd.FileNames)
+                {
+                    if (s.IndexOf("xspworkspace\\") == -1)
+                    {
+                        DialogResult dr = MessageBox.Show("请选择位于xspworkspace目录下的源文件！", "加载源文件");
+                        richTextBoxLoadFile.Text = "";
+                        richTextBoxEncryptFile.Text = "";
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void ButtonEncrypt_Click(object sender, EventArgs e)
+        {
+            foreach (string s in filePaths)
+            {
+                if (s== null)
+                {
+                    break;
+                }
+
+                string cmd = "luac -o " + Path.GetFullPath(s + ".tmp") + " " + Path.GetFullPath(s);
+                executeCmd(cmd);
+
+                try
+                {
+                    FileStream fssrc = new FileStream(Path.GetFullPath(s + ".tmp"), FileMode.Open);
+                    BinaryReader brsrc = new BinaryReader(fssrc);
+
+                    byte[] bsrc = new byte[fssrc.Length + 8];
+                    int index = 0;
+
+                    while (true)
+                    {
+                        try
+                        {
+                            byte b = brsrc.ReadByte();
+                            bsrc[index] = b;
+                            index++;
+                        }
+                        catch (EndOfStreamException)
+                        {
+                            break;
+                        }
+                    }
+
+                    string encryptedStr = Convert.ToBase64String(bsrc);
+                    encryptedStr = "return \"" + encryptedStr + "\"";
+                    byte[] encryptedByte = Encoding.UTF8.GetBytes(encryptedStr);
+
+                    string path = Path.GetDirectoryName(s) + "\\" + Path.GetFileNameWithoutExtension(s) + "_" + Path.GetExtension(s);
+
+                    FileStream fsdst = new FileStream(path, FileMode.Create);
+                    BinaryWriter bwdst = new BinaryWriter(fsdst);
+
+                    bwdst.Write(encryptedByte);
+
+                    bwdst.Close();
+                    fsdst.Close();
+                    brsrc.Close();
+                    fssrc.Close();
+
+                    richTextBoxEncryptFile.AppendText(Path.GetFileName(s) + "    加密成功  ");
+
+                    string bakRootDir = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\xspBackup\\";
+                    string currentSubDir = Path.GetDirectoryName(s).Substring(Path.GetDirectoryName(s).LastIndexOf("xspworkspace\\") + 13) + "\\";
+                    Console.WriteLine(bakRootDir + currentSubDir);
+                    if (!Directory.Exists(bakRootDir + currentSubDir))
+                    {
+                        Directory.CreateDirectory(bakRootDir + currentSubDir);
+                    }
+                    if (!Directory.Exists(bakRootDir + "static_backup\\" + currentSubDir))
+                    {
+                        Directory.CreateDirectory(bakRootDir + "static_backup\\" + currentSubDir);
+                    }
+
+                    try
+                    {
+                        File.Copy(Path.GetFullPath(s), bakRootDir + "static_backup\\" + currentSubDir + Path.GetFileName(s), true);
+                        File.Copy(Path.GetFullPath(s), bakRootDir + currentSubDir + Path.GetFileName(s), true);
+                        File.Delete(Path.GetFullPath(s));
+                        richTextBoxEncryptFile.AppendText("备份成功\r\n");
+                    }
+                    catch(Exception)
+                    {
+                        richTextBoxEncryptFile.AppendText("备份失败\r\n");
+                    }
+                }
+                catch (FileNotFoundException)
+                {
+                    DialogResult dr = MessageBox.Show("未找到中间文件，请确保安装好lua5.1！", "加密原文件");
+                    richTextBoxEncryptFile.AppendText(Path.GetFileName(s) + "备份失败\r\n");
+                }
+                finally
+                {
+                    if (File.Exists(Path.GetFullPath(s + ".tmp")))
+                    {
+                        File.Delete(Path.GetFullPath(s + ".tmp"));
+                    }
+                }
+            }
+        }
+
+        public static void CopyDirectory(String sourcePath, String destinationPath)
+        {
+            DirectoryInfo info = new DirectoryInfo(sourcePath);
+            Directory.CreateDirectory(destinationPath);
+            foreach (FileSystemInfo fsi in info.GetFileSystemInfos())
+            {
+                String destName = Path.Combine(destinationPath, fsi.Name);
+
+                if (fsi is System.IO.FileInfo)          //如果是文件，复制文件 
+                    File.Copy(fsi.FullName, destName, true);
+                else                                    //如果是文件夹，新建文件夹，递归 
+                {
+                    Directory.CreateDirectory(destName);
+                    CopyDirectory(fsi.FullName, destName);
+                }
+            }
+        }
+
+        //删除目录下的所有文件及文件夹
+        private static void deleteDirFiles(string strPath)
+        {
+            //删除这个目录下的所有子目录
+            if (Directory.GetDirectories(strPath).Length > 0)
+            {
+                foreach (string var in Directory.GetDirectories(strPath))
+                {
+                    Directory.Delete(var, true);
+                }
+            }
+            //删除这个目录下的所有文件
+            if (Directory.GetFiles(strPath).Length > 0)
+            {
+                foreach (string var in Directory.GetFiles(strPath))
+                {
+                    File.Delete(var);
+                }
+            }
+        }
+
+        private void ButtonRecover_Click(object sender, EventArgs e)
+        {
+ 
+            string bakDir = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\xspBackup\\";
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.SelectedPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\xspBackup\\";
+
+            dialog.Description = @"请选择\xspBackup\目录下要还原的项目文件夹";
+            DialogResult result = dialog.ShowDialog();
+            if (result == System.Windows.Forms.DialogResult.Cancel)
+            {
+                return;
+            }
+            if (dialog.SelectedPath.IndexOf("xspBackup\\") == -1)
+            {
+                MessageBox.Show(@"必须选择\xspBackup\目录下要还原的项目文件夹");
+                return;
+            }
+
+            string selectPath = dialog.SelectedPath;
+            Console.WriteLine(selectPath);
+            string dstPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\xspworkspace\\";
+            dstPath += Path.GetFullPath(selectPath).Substring(selectPath.LastIndexOf("xspBackup\\") + ("xspBackup\\").Length);
+            Console.WriteLine(dstPath);
+
+            CopyDirectory(selectPath, dstPath);
+
+            deleteDirFiles(selectPath);
+
+            MessageBox.Show("还原成功！");
+        }
+    }
+}
